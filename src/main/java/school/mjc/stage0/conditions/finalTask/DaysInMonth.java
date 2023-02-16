272483a9ea89d21b088f5b403fb821fdb579e93c
ulvertrom <ulvertrom@gmail.com>
2023-02-16 23:40:06 +0400

    6 progs added

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -2,6 +2,55 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class DaysInMonth {
     public void printDays(int year, int month) {
+        int remainder = year % 4;
+        int leap = 0;
+        switch (remainder) {
+            case 1:
+            case 2:
+            case 3:
+                leap = 0;
+                break;
+            case 0:
+                if ((year % 100) != 0) {
+                    leap = 1;
+                }
+                else {
+                    if ((year % 400) == 0) {
+                        leap = 1;
+                    } else {
+                        leap = 0;
+                    }
+                }
+                break;
+        }
+        //System.out.println(leap);
+        if (year >= 0) {
+            switch (month) {
+                case (1):
+                case (3):
+                case (5):
+                case (7):
+                case (8):
+                case (10):
+                case (12):
+                    System.out.println("31");
+                    break;
+                case (4):
+                case (6):
+                case (9):
+                case (11):
+                    System.out.println("30");
+                    break;
+                case (2):
+                    System.out.println(leap == 0 ? "28" : "29");
+                    break;
+                default:
+                    System.out.println("invalid date");
+                    break;
+            }
+        } else {
+            System.out.println("invalid date");
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int quotient;
+        if (divider != 0) {
+            quotient = dividend / divider;
+            System.out.println(dividend == (quotient * divider) ? "can be divided completely": "cannot be divided completely");
+        } else {
+            System.out.println("division by zero");
+        }
+
     }
 }
